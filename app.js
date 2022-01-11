d15c70baaec9630cd0a0fef20e988edcf4897839
Julien <j.keiff@hotmail.fr>
2022-01-11 18:36:46 +0100

    fix(size): Remove unused depandance

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const bodyParser = require('body-parser')
-
 const app = express();
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-// static Images Folder
-app.use('/uploads', express.static('./uploads'))
 
 module.exports = app;
